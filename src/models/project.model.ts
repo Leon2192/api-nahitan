1854eafbe74bd47de015163b5f78bd6ef5a48361
leon_2192 <leon.strokes@gmail.com>
2023-10-10 09:43:14 -0300

    fix model

--- src/models/project.model.ts
+++ src/models/project.model.ts
@@ -4,6 +4,9 @@ const projectSchema = new Schema({
   name: { type: String, required: true },
   description: { type: String },
   technology: { type: String },
+  image: { type: String },
+  githug: { type: String },
+  source: { type: String },
 });
 
 export default model("Project", projectSchema);
