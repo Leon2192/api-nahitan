c95ae394733470a57218a9d501528152d8456d62
leon_2192 <leon.strokes@gmail.com>
2023-10-10 15:49:56 -0300

    fix cors

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,18 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import projectRoutes from "./routes/project.routes";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "https://44.216.124.74/",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
 
 app.use(express.json());
 app.use(projectRoutes);
